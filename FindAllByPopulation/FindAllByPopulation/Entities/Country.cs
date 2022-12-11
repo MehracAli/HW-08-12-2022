166772db082da60afd8916813bcd62f98c54e2cd
Mehrac Mammadov <mehrajvm@code.edu.az>
2022-12-11 18:55:57 +0400

    methods upgraded

--- FindAllByPopulation/FindAllByPopulation/Entities/Country.cs
+++ FindAllByPopulation/FindAllByPopulation/Entities/Country.cs
@@ -12,7 +12,7 @@ namespace FindAllByPopulation.Entities
         List<City> Cities;
         static int count;
 
-        public Country(string name) { Name = name; Cities = new List<City>(); }
+        public Country(string name) { Name = name; Cities = new List<City>(); Console.WriteLine($"Country: {name}\n") ; }
 
         public void AddCity(City city) { Cities.Add(city); }
 
@@ -23,20 +23,22 @@ namespace FindAllByPopulation.Entities
                 City city = Cities[i];
                 count+=city.Population;
             }
-                Console.WriteLine(count);
+                Console.WriteLine($"Total population: {count} people.");
         }
 
         public void  FindAllByPopulation(int min, int max)
         {
+            Console.WriteLine($"Cities in interval {min}-{max}: \n");
+
             for (int i = 0; i < Cities.Count; i++)
             {
                 City city = Cities[i];
-
                 if (city.Population >= min && city.Population <= max)
                 {
-                    Console.WriteLine(city.Name);
+                    Console.WriteLine("- "+city.Name);
                 }
             }
+            Console.WriteLine(" ");
         }
 
     }
